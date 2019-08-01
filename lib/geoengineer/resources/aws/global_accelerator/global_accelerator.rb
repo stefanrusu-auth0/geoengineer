f8924b1803dcd27055c0f174fc9e5a3693f20712
Jared Koumentis <jared.koumentis@auth0.com>
2019-07-31 21:12:43 -0400

    Add more to the ga resource

--- lib/geoengineer/resources/aws/global_accelerator/global_accelerator.rb
+++ lib/geoengineer/resources/aws/global_accelerator/global_accelerator.rb
@@ -1,7 +1,7 @@
 class GeoEngineer::Resources::GlobalAccelerator < GeoEngineer::Resource
   validate -> { validate_required_attributes([:name, :ip_address_type, :enabled]) }
 
-  after :initialize, -> { _terraform_id -> { name } }
+  after :initialize, -> { _terraform_id -> { NullObject.maybe(remote_resource)._terraform_id } }
 
   def to_terraform_state
     tfstate = super
@@ -14,5 +14,23 @@ class GeoEngineer::Resources::GlobalAccelerator < GeoEngineer::Resource
     tfstate[:primary][:attributes]['filename'] = filename if filename
 
     tfstate
+  end
+
+  def short_type
+    "ga"
+  end
+
+  def support_tags?
+    false
+  end
+
+  def self._fetch_remote_resources(provider)
+    client = AwsClients.accelerator(provider)
+    client.describe_accelerator['accelerator'].map(&:to_h).map do |ga|
+      ga[:_terraform_id] = ga[:accelerator_arn]
+      ga[:_geo_id] = "#{ga[:accelerator_arn]}::#{ga[:type]}"
+      ga[:_arn] = ga[:accelerator_arn]
+      ga
+    end
   end
 end
